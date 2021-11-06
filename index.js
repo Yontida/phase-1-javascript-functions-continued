b56c4a02149e2fd859e94f810e2d63133e09eaba
Yontida Yosbut <a.yontida@gmail.com>
2021-11-05 23:14:01 -0400

    commit tree ead034a376a3d5e7de24b4414c81468c3e924ba3

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+
+// uses the default activity 'roller-skate' when no arguments are passed
+function saturdayFun(activity = 'roller-skate'){
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(work = 'go to the office'){
+  return `This Monday, I will ${work}.`;
+}
+
+// #1 method
+function wrapAdjective (flair = "*"){
+  const innerFunction = function (adj = "special"){
+    return `You are ${flair}${adj}${flair}!`
+  };
+  return innerFunction;
+}
+
+// #2 method
+// function wrapAdjective (flair = "*"){
+//   return function innerFunction (adj = "special"){
+//     return `You are ${flair}${adj}${flair}!`
+//   } 
+// }
+
+// console.log(wrapAdjective("%")("a dedicated programmer"));
